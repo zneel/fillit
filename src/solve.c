fecb8e8a556d4f8adb0558ddf9860b15ec3a883d
Sacha REQUIEM <srequiem@e1r9p13.42.fr>
2018-04-15 15:13:32 +0200

    Marvin la pomme de terre

--- src/solve.c
+++ src/solve.c
@@ -6,19 +6,46 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 21:16:11 by ebouvier          #+#    #+#             */
-/*   Updated: 2018/04/15 13:40:56 by ebouvier         ###   ########.fr       */
+/*   Updated: 2018/04/15 15:13:05 by srequiem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-t_map    *ft_solve(t_tris *tetriminos_list)
+char	*ft_fillmap(char *str, int size)
 {
-    t_map   *map;
+	int		i;
+	int		count;
 
+	i = 0;
+	count = 0;
+	while (i < size - 1)
+	{
+		if (count == 4)
+		{
+			str[i] = '\n';
+			count = -1;
+		}
+		else
+			str[i] = '.';
+		i++;
+		count++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+t_map    *ft_solve(t_tris *tetriminos_list, uint16_t size)
+{
+    t_map   	*map;
+	uint16_t	len;
+
+	len = (size * size) + size + 1;
     map = (t_map*)malloc(sizeof(t_map));
+	if (!(map->str = (char*)malloc(len * sizeof(char))))
+		return (0);
+	
     (void) tetriminos_list;
-    
+
     return (map);
 }
 
